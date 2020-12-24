80ca76e6bb8e2d963530c489df910b3cb2df13e6
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 09:50:02 +0100

    Ejercicio 2

---
+++ UD04_Ej2/UD04_Ej2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UD04_Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu ciudad:");
+            string ciudad = Console.ReadLine();
+
+            // He decidido por este método de mostrar variables ya que me parece más cómodo.
+            Console.WriteLine("Hola {0} bienvenido a {1}", nombre, ciudad);
+
+        }
+    }
+}
