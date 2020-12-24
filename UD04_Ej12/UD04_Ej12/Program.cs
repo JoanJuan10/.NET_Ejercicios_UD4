09e67988d80fdb0d16f2fc974e059ad34fd10bae
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 10:27:09 +0100

    Ejercicio 12

---
+++ UD04_Ej12/UD04_Ej12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UD04_Ej12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Fernando";
+
+            Console.WriteLine("Bienvenido {0}", nombre);
+        }
+    }
+}
