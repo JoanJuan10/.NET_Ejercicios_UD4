b4141b26e687a6cb42f19b6584851bd7e745a709
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 10:25:50 +0100

    Ejercicio 11

---
+++ UD04_Ej11/UD04_Ej11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UD04_Ej11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 9;
+            int num2 = 10;
+
+            if (num1 < num2)
+            {
+                Console.WriteLine("El numero 2 es mas grande");
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Los dos numeros son iguales");
+            }
+            else
+            {
+                Console.WriteLine("El numero 1 es mas grande");
+            }
+        }
+    }
+}
