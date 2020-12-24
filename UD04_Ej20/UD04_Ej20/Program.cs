fcc5b8fca41d3e3199d3977a0fd06e6486bfec71
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 11:00:17 +0100

    Ejercicio 20

---
+++ UD04_Ej20/UD04_Ej20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UD04_Ej20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pedimos el numero de ventas
+            Console.WriteLine("Introduce el numero de ventas que has obtenido");
+            string numVentasString = Console.ReadLine();
+
+            int numVentas = Convert.ToInt32(numVentasString);
+            int[] ventas = new int[numVentas];
+
+            // Por cada venta, pedimos lo recaudado y lo guardamos
+            for (int i = 0; i < numVentas; i++)
+            {
+                Console.WriteLine("Introduce la venta {0}", (i + 1));
+                string ventaString = Console.ReadLine();
+
+                ventas[i] = Convert.ToInt32(ventaString);
+
+            }
+            int sumVentas = 0;
+            // Por cada venta, lo sumamos al total.
+            for (int i = 0; i < numVentas; i++)
+            {
+                sumVentas += ventas[i];
+            }
+            Console.WriteLine("Total en ventas: {0} euros", sumVentas);
+        }
+    }
+}
