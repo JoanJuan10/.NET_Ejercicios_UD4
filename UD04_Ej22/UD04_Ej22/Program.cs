fbdd15ac104224399d590d510cd04566ade9fd13
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 11:11:26 +0100

    Ejercicio 22

---
+++ UD04_Ej22/UD04_Ej22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UD04_Ej22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1234";
+            bool acertada = false;
+            int intentos = 3;
+
+            while (!acertada && intentos > 0)
+            {
+                Console.WriteLine("Introduce la contraseña ({0} intentos)", intentos);
+
+                string contra = Console.ReadLine();
+
+                if (contra == password)
+                {
+                    Console.WriteLine("Enhorabuena!!");
+                    acertada = true;
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña incorrecta");
+                    intentos--;
+                }
+            }
+            
+        }
+    }
+}
