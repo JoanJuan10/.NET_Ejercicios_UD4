65608427d3196c6ac3b4da1c6cecb2a776b2c778
JoanJuan10 <iijoanjuan10@gmail.com>
2020-12-24 10:28:11 +0100

    Ejercicio 13

---
+++ UD04_Ej13/UD04_Ej13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD04_Ej13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido {0}", nombre);
+        }
+    }
+}
